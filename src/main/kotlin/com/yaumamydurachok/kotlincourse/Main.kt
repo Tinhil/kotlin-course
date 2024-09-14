7f7207ed74862fa82cb9f54e3063817230acf27a
Tinhil <a.j.haatainen@gmail.com>
2024-09-15 00:36:50 +0300

    fourth homework

--- src/main/kotlin/com/yaumamydurachok/kotlincourse/Main.kt
+++ src/main/kotlin/com/yaumamydurachok/kotlincourse/Main.kt
@@ -19,3 +19,27 @@ var fuel: Double = 0.0
     set(value) {
         if (value > 0) field += value
     }
+
+val myInt: Int = 2147483647
+val myFloat: Float = 0.58F
+val myLong: Long = 1_234_567_890L
+val myShort: Short = 32000
+val myByte: Byte = 120
+val myDouble: Double = 5.99
+
+val isKotlinFun: Boolean = true
+
+val letter: Char = 'A'
+
+val numbers: Array<Int> = arrayOf(1, 2, 3)
+val strings: List<String> = listOf("one", "two", "three")
+val doubles: Set<Double> = setOf(23.3, 56.8)
+
+val keysToValues: Map<String, String> = mapOf(
+    "Ключ 1" to "Значение 1",
+    "Ключ 2" to "Значение 2",
+)
+
+val anything: Any = "Ыть"
+
+//ещё тут Unit и Nothing
