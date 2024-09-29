73f08c7a27e351230247dec2fc72569fbc336e34
Tinhil <a.j.haatainen@gmail.com>
2024-09-30 02:19:39 +0300

    8th homework, будь она неладна

--- src/main/kotlin/com/yaumamydurachok/kotlincourse/lesson8/homework8.kt
+++ src/main/kotlin/com/yaumamydurachok/kotlincourse/lesson8/homework8.kt
@@ -1,21 +1,55 @@
-package com.yaumamydurachok.kotlincourse.lesson8
-
-
-fun quest1() {
-    val loginDateTime = "Пользователь вошёл в систему -> 2021-12-01 09:48:23"
-    val splitDateTime = loginDateTime.split(" -> ")
-    print(splitDateTime)
-}
-
-fun quest2() {
-    val digits = "4539 1488 0343 6467"
-    val newDigits = digits.replace("4539 1488 0343","**** **** ****")
-    print (newDigits)
-}
-
-fun quest3 () {
-    val soap = "username@example.com"
-    val soap1 = soap.replace("@", "[at]")
-    val soap2 = soap1.replace(".", "dot")
-    print(soap2)
-}
+//package com.yaumamydurachok.kotlincourse.lesson8
+//
+//fun texts() {
+//    val text1 = " "
+//    if {
+//        text1.contains("невозможно")
+//        text1.replace("невозможно", "совершенно точно возможно, просто требует времени")
+//    } else if {
+//        text1.startsWith("Я не уверен")
+//        print($"text1", "но моя интуиция говорит об обратном")
+//    } else if {
+//        text1.contains("катастрофа")
+//        text1.replace("катастрофа", "интересное событие")
+//    } else if {
+//        text1.endsWith("без проблем")
+//        text1.replace("без проблем", "с парой интересных вызовов на пути")
+//    } else {
+//        text1.isNotC
+//    }
+//
+//}
+//
+//fun quest1() {
+//    val loginDateTime = "Пользователь вошёл в систему -> 2021-12-01 09:48:23"
+//    val splitDateTime = loginDateTime.split(" -> ")
+//    print(splitDateTime)
+//}
+//
+//fun quest2() {
+//    val digits = "4539 1488 0343 6467"
+//    val newDigits = digits.replace("4539 1488 0343","**** **** ****")
+//    print (newDigits)
+//}
+//
+//fun quest3 () {
+//    val soap = "username@example.com"
+//    val soap1 = soap.replace("@", "[at]")
+//    val soap2 = soap1.replace(".", "[dot]")
+//    print(soap2)
+//}
+//
+//fun quest4 () {
+//    val theWay = "C:/Пользователи/Документы/report.txt"
+//    val fileName = theWay.split("/")
+//}
+//
+//fun quest5 () {
+//    val abb = "Завод крупно-панельного домостроения"
+//    val wrds = abb.split(" ", "-")
+//    for (i in wrds) {
+//        var lttr = i.substring(0, 1)
+//    }
+//    val itog = lttr.upperCase
+//    print(itog)
+//}
