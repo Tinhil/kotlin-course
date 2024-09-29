8221012356ef8ac69590c71739923925ed8c0a83
Tinhil <a.j.haatainen@gmail.com>
2024-09-29 15:23:06 +0300

    7th homework

---
+++ src/main/kotlin/com/yaumamydurachok/kotlincourse/lesson7/homework7.kt
@@ -0,0 +1,50 @@
+package com.yaumamydurachok.kotlincourse.lesson7
+
+fun main() {
+//    for (i in 1..5) {
+//        println(i)
+//    }
+//
+//    for (i in 1..10) {
+//        if (i % 2 == 0)
+//        println(i)
+//    }
+//
+//    for (i in 5 downTo 1) {
+//        println(i)
+//    }
+//
+//    for (i in 10 downTo 1 step 2) {
+////        i -= 2
+//        println(i - 2)
+//    }
+//
+//    for (i in 1..9 step 2) {
+//        println(i)
+//    }
+//
+//    for (i in 1..20) {
+//        if (i % 3 != 0) continue
+//        println(i)
+//    }
+//
+//    for (i in 1 until 9) {
+//        println(i)
+//    }
+//
+//    for (i in 3 until 15) {
+//        println(i)
+//    }
+//
+//    var sq = 0
+//    while (sq++ < 5) {
+//        println (sq * sq)
+//    }
+//
+//    var minus = 10
+//    while (minus-- >5) {
+//        println(minus)
+//    }
+
+
+}
---
+++ src/main/kotlin/com/yaumamydurachok/kotlincourse/lesson7/lesson.kt
@@ -0,0 +1,13 @@
+//package com.yaumamydurachok.kotlincourse.lesson7
+//
+//fun main() {
+//    for (item in collection) {
+//        //тело цикла
+//    }
+//    while (condition) {
+//        //тело цикла
+//    }
+//    do {
+//        //тело цикла
+//    } while (collection)
+//}
